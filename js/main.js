1fe4ec66880695595df6a6d82a7d166d7512d84c
MacBook Pro18 <macbookpro18@MacBooks-MacBook-Pro-3.local>
2018-05-16 16:29:10 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
-console.log("Up and running!");
-var cardOne = 'Queen';
-var cardTwo = 'Queen';
-var cardThree = 'King';
-var cardFour = 'King';
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+//memory game project 
 
+//creating array cards to store face values of the cards
+var cards = ["Queen", "Queen", "King", "King"];
+var cardsInPlay = [];
+//variables for cards flipped
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardsInPlay);
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardsInPlay);
+//game logic to see if two cards have been played
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, but try again");
+	}
+}
