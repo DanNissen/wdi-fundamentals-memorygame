ab13afcc9f85b46d62d7f33b73c4d646d29891fc
MacBook Pro18 <macbookpro18@MacBooks-MacBook-Pro-3.local>
2018-05-16 20:58:45 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,28 @@
 //memory game project 
 
 //creating array cards to store face values of the cards
-var cards = ["Queen", "Queen", "King", "King"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-diamonds.png"
+},
+];
 var cardsInPlay = [];
 //function to check for a match 
 var checkForMatch = function(){
@@ -14,8 +35,10 @@ var checkForMatch = function(){
 
 //function to store logic of when user flips card
 var flipCard = function(cardId){
-	console.log("User Fliped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]); 
+	console.log("User Fliped " + cards[cardId].rank);
+	console.log("User Fliped " + cards[cardId].suit); 
+	console.log("User Fliped " + cards[cardId].cardImage);
+	cardsInPlay.push(cards[cardId].rank); 
 	if (cardsInPlay.length === 2) {
 		checkForMatch();}	
 	}
