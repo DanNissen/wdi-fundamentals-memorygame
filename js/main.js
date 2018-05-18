664875134ecf399491a37126148436682f9d67d2
MacBook Pro18 <macbookpro18@MacBooks-MacBook-Pro-3.local>
2018-05-17 20:55:34 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -34,15 +34,28 @@ var checkForMatch = function(){
 }
 
 //function to store logic of when user flips card
-var flipCard = function(cardId){
+var flipCard = function(){
+	var cardId = this.getAttribute('data-id');
 	console.log("User Fliped " + cards[cardId].rank);
 	console.log("User Fliped " + cards[cardId].suit); 
 	console.log("User Fliped " + cards[cardId].cardImage);
-	cardsInPlay.push(cards[cardId].rank); 
+	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage)
 	if (cardsInPlay.length === 2) {
 		checkForMatch();}	
 	}
 
-flipCard(0);
-flipCard(2);
+var createBoard = function(){
+	for (var i=0; i < cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener("click", flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+createBoard();
+
+
 
